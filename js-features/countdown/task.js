4ee93e12ce727bd1e52cd8a04c8955a7d6af6790
Dmitry.Kononets <d.cononec@gmail.com>
2023-05-20 18:36:56 +0300

    две задачи

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -6,3 +6,26 @@ let timer = setInterval(() => {
     clearInterval(timer);
   }
 }, 1000);
+
+const counterTwo = document.getElementById('timerTwo');
+let HH = document.getElementById('HH'),
+    MM = document.getElementById('MM'),
+    SS = document.getElementById('SS');
+let timerTwo = setInterval(() => {
+  +SS.textContent--
+  if(+SS.textContent <= 9 || +SS.textContent <= 0 ){
+    SS.textContent = SS.textContent.padStart(2, '0')
+  } else if(+MM.textContent <= 9 || +MM.textContent <= 0){
+    MM.textContent = MM.textContent.padStart(2, '0')
+  }
+  if(+SS.textContent <= 0){
+    +MM.textContent--;
+    SS.textContent = 60;
+  } else if(+MM.textContent <= 0){
+    HH.textContent--;
+    MM.textContent = 59;
+  } else if((+HH.textContent + +MM.textContent + +SS.textContent) === 0){
+    alert('Время вышло!')
+    clearInterval(timer);
+  }
+}, 1000);
