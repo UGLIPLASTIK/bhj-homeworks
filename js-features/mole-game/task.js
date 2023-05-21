09dde16ee229ef432c5ff3703e6a02030ad899a7
Dmitry.Kononets <d.cononec@gmail.com>
2023-05-21 04:35:29 +0300

    готово

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -8,6 +8,22 @@ getHole = index => document.getElementById(`hole${index}`);
 
 for(let holeIndex = 1; holeIndex < 10; holeIndex++){
   let hole = getHole(holeIndex);
-  hole.addEventListener
-  console.log(hole)
+  hole.onclick = function(){
+    if(hole.classList.contains('hole_has-mole')){
+      counterDead++;
+    }else{
+      counterLost++;
+    }
+    if(counterDead === 10){
+      alert('Win!');
+      counterDead = 0;
+      counterLost = 0;
+    }else if(counterLost === 5){
+      alert('Lost!')
+      counterDead = 0;
+      counterLost = 0;
+    }
+    dead.textContent = counterDead;
+    lost.textContent = counterLost;
+  }
 }
