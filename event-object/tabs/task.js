e1a3ae123841b6b7ee69d87fbb46798d8a8b9952
Dmitry.Kononets <d.cononec@gmail.com>
2023-06-01 18:05:23 +0300

    задача 3 готова

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -6,10 +6,12 @@ const contentArr = Array.from(document.querySelectorAll('.tab__content'));
 tabArr.forEach(item => {
   item.addEventListener('click', (event) => {
     tabArr.forEach(item => {
-      item.classList.remove('tab_active')
-    })
+      item.classList.remove('tab_active');
+    });
     event.currentTarget.classList.toggle('tab_active');
-    console.log(tabArr.indexOf(item))
-    // contentArr[tabArr.indexOf(item)].classList.toggle('tab__content_active')
+    contentArr.forEach(item => {
+      item.classList.remove('tab__content_active');
+    });
+    contentArr[tabArr.indexOf(item)].classList.toggle('tab__content_active');
   })
 })
