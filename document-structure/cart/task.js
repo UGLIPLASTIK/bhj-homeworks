b1b48df0b185a21f4737ffd6348962e693f7a3ba
Dmitry.Kononets <d.cononec@gmail.com>
2023-06-19 20:49:40 +0300

    Update task.js

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,23 +1,28 @@
 const cart = document.querySelector('.cart__products');
 const quantityControls = Array.from(document.querySelectorAll('.product__quantity-control'));
 const quantityValue = document.querySelector('.product__quantity-value');
-const productArr = Array.from(document.querySelectorAll('.product'));
-productArr.forEach(item => {
-  item.addEventListener('click', () => {
-    item.querySelector('.product__quantity-value');
+const addBtns = document.querySelectorAll('.product__add');
+
+addBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const block = `
+      <div class="cart__product" data-id="1">
+        <img class="cart__product-image" src="">
+        <div class="cart__product-count">20</div>
+      </div>`
+    cart.insertAdjacentHTML('beforeEnd', block);
+    let img = btn.closest('.product').querySelector('img').cloneNode(true);
+    let newDataId = btn.closest('.product').getAttribute('data-id');
+    cart.lastChild.replaceChild(img, cart.lastChild.children[0]);
+    cart.lastChild.querySelector('.cart__product-count').textContent = btn.previousElementSibling.querySelector('.product__quantity-value').textContent;
+    cart.lastChild.setAttribute('data-id', newDataId)
   })
 })
 
-// function changeValue(item){
-//   if(item.parentElement.querySelector('.product__quantity-value').textContent >= 1) return false;
-//   if (item.classList.contains('product__quantity-control_dec')){
-//     return item.parentElement.querySelector('.product__quantity-value').textContent--;
-//   } else return item.parentElement.querySelector('.product__quantity-value').textContent++;
-// } 
-
-quantityControls.forEach(item =>{
+quantityControls.forEach(item => {
   item.addEventListener('click', () => {
     if(item.classList.contains('product__quantity-control_dec') && item.parentElement.querySelector('.product__quantity-value').textContent != 1) return item.parentElement.querySelector('.product__quantity-value').textContent--;
     else if (item.classList.contains('product__quantity-control_inc')) return item.parentElement.querySelector('.product__quantity-value').textContent++;
   })
 })
+
