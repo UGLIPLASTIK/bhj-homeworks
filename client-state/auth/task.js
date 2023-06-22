0932036c3939aca292e3a82ec41ca488551cccc5
Dmitry.Kononets <d.cononec@gmail.com>
2023-06-22 16:38:43 +0300

    задача 3 готова

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,7 +1,27 @@
-document.forms.form.addEventListener('submit', (e) => {
+let userId = localStorage.getItem('user_id');
+
+if(userId){
+  welcome.classList.add('welcome_active');
+  welcome.querySelector('span').textContent = localStorage.getItem('user_id');
+}
+
+document.forms.signin__form.addEventListener('submit', (e) => {
   e.preventDefault();
   const xhr = new XMLHttpRequest();
   xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
-  const formData = new FormData(document.forms.form)
+
+  xhr.onload = function() {
+    const data = JSON.parse(xhr.response);
+    const welcome = document.getElementById('welcome');
+
+    if(data.success) {
+      localStorage.setItem('user_id', data.user_id);
+      
+      welcome.classList.add('welcome_active');
+      welcome.querySelector('span').textContent = data.user_id;
+    } else alert('Неверный логин/пароль');
+  }
+
+  const formData = new FormData(document.forms.signin__form);
   xhr.send(formData);
 })
