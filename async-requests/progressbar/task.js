7716d1be25c9788753b55d2cae5098a238b4fda5
Dmitry.Kononets <d.cononec@gmail.com>
2023-07-08 18:05:18 +0300

    Update task.js

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -3,10 +3,14 @@ const progress = document.getElementById('progress');
 document.forms.form.addEventListener('submit', (e) => {
   e.preventDefault();
   const xhr = new XMLHttpRequest();
-  xhr.addEventListener('readystatechange', () => {
-    progress.value = xhr.readyState / 4;
-  })
+
+  xhr.upload.onprogress = function(event) {
+    // console.log(`Отправлено ${event.loaded} из ${event.total}`);
+    // console.log(event.loaded)
+
+  };
+
   xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload');
-  const formData = new FormData(document.forms.form)
+  const formData = new FormData(document.forms.form);
   xhr.send(formData);
 })
